e7425580df1be86269ee54f938c9a018e88f1be6
EAMendiguzhaev <mendiguzhaevea@gmail.com>
2021-06-01 20:20:59 +0300

    Выполняю домашнее задание 4 урока

--- script.js
+++ script.js
@@ -1,54 +1,17 @@
 "use strict";
 
-// Пункт №1 домашнего задания
+const alertQuestion = prompt("Введите любое предложение");
 
-let lang = "ru";
-let array = [];
+const puzzleFunction = function (randomValue) {
+  const size = 31;
 
-if (lang === "ru") {
-  array = ["пн", "вт", "ср", "чт", "пт", "сб", "вс"];
-} else if (lang === "en") {
-  array = ["mn", "ts", "wd", "th", "fr", "st", "sn"];
-}
-
-console.log("array: ", array);
-
-switch (lang) {
-  case "ru":
-    array = ["пн", "вт", "ср", "чт", "пт", "сб", "вс"];
-    break;
-  default:
-    alert("Нет таких значений");
-}
-
-switch (lang) {
-  case "en":
-    array = ["mn", "ts", "wd", "th", "fr", "st", "sn"];
-    break;
-  default:
-    alert("There are no such values");
-}
-
-console.log("array: ", array);
-
-let object = {
-  ru: ["пн", "вт", "ср", "чт", "пт", "сб", "вс"],
-  en: ["mn", "ts", "wd", "th", "fr", "st", "sn"],
+  if (+randomValue) {
+    alert("Вы ввели число!");
+  } else if (randomValue.length <= 30) {
+    console.log(randomValue.trim());
+  } else if (randomValue.length > 30) {
+    console.log(randomValue.trim().slice(0, size) + "...");
+  }
 };
 
-array = object[lang];
-
-console.log(array);
-
-// Пункт №2 домашнего задания
-
-let message = prompt("Введите имя Артём или Максим");
-
-let namePerson =
-  message === "Артём"
-    ? alert("Директор")
-    : message === "Максим"
-    ? alert("Преподаватель")
-    : message === ""
-    ? alert("Студент")
-    : alert("Студент");
+puzzleFunction(alertQuestion);
