2a7f1d7701b4535c3c65c0e0369b5d0b3504e81d
EAMendiguzhaev <mendiguzhaevea@gmail.com>
2021-06-01 22:07:54 +0300

    Отредактировал код после проверки ДЗ

--- script.js
+++ script.js
@@ -7,11 +7,31 @@ const puzzleFunction = function (randomValue) {
 
   if (+randomValue) {
     alert("Вы ввели число!");
-  } else if (randomValue.length <= 30) {
-    console.log(randomValue.trim());
-  } else if (randomValue.length > 30) {
-    console.log(randomValue.trim().slice(0, size) + "...");
+    return;
   }
+
+  randomValue = randomValue.trim();
+  return randomValue.length > 30
+    ? console.log(randomValue.trim().slice(0, size) + "...")
+    : console.log(randomValue);
 };
 
 puzzleFunction(alertQuestion);
+
+// Предыдущий метод
+
+// const alertQuestion = prompt("Введите любое предложение");
+
+// const puzzleFunction = function (randomValue) {
+//   const size = 31;
+
+//   if (+randomValue) {
+//     alert("Вы ввели число!");
+//   } else if (randomValue.length <= 30) {
+//     console.log(randomValue.trim());
+//   } else if (randomValue.length > 30) {
+//     console.log(randomValue.trim().slice(0, size) + "...");
+//   }
+// };
+
+// puzzleFunction(alertQuestion);
