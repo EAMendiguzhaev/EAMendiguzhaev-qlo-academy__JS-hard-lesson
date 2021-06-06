72253b5652c1a2dcae72090243e27b41bd9b3aed
EAMendiguzhaev <mendiguzhaevea@gmail.com>
2021-06-06 21:25:27 +0300

    Отредактировал после проверки

--- script.js
+++ script.js
@@ -1,56 +1,54 @@
-"use strict";
+'use strict';
 
-//Создаю массив week и записвыю в него дни недели в виде строк
-
-const week = [
-  "Понедельник",
-  "Вторник",
-  "Среда",
-  "Четверг",
-  "Пятница",
-  "Суббота",
-  "Воскресенье",
+let days = [
+  'Понедельник',
+  'Вторник',
+  'Среда',
+  'Четверг',
+  'Пятница',
+  '<i>Суббота</i>',
+  '<i>Воскресенье</i>',
 ];
 
-const day = document.querySelector(".week");
-const todayDay = new Date("June 05, 2021 21:21:21");
+let todayIndex = (6 + new Date().getDay()) % 7;
+days[todayIndex] = `<b>${days[todayIndex]}</b>`;
 
-const days = () => {
-  week.forEach((item, i) => {
-    let newdiv = document.createElement("div");
+document.body.insertAdjacentHTML('beforeEnd', days.join('<br>'));
 
-    if (i === +todayDay.getDay() - 1) {
-      console.log(todayDay.getDay());
-      newdiv.classList.add("bold");
-      newdiv.textContent = week[i];
-    }
+// Другой подход
 
-    if (item === "Суббота" || item === "Воскресенье") {
-      newdiv.classList.add("italic"); // Делаем дополнительно курсивом (Даже если уже жирным выделено)
-      newdiv.textContent = week[i];
-    } else {
-      newdiv.textContent = week[i]; // Если не текущий день и не выходные, то просто вставляем текст без стилей
-    }
-
-    day.appendChild(newdiv);
-  });
-};
-
-days();
-
-// Этот читерный вариант в интернете нашел =)
-
-// let days = [
-//   "Понедельник",
-//   "Вторник",
-//   "Среда",
-//   "Четверг",
-//   "Пятница",
-//   "<i>Суббота</i>",
-//   "<i>Воскресенье</i>",
+// const week = [
+//   'Понедельник',
+//   'Вторник',
+//   'Среда',
+//   'Четверг',
+//   'Пятница',
+//   'Суббота',
+//   'Воскресенье',
 // ];
 
-// let todayIndex = (6 + new Date().getDay()) % 7;
-// days[todayIndex] = `<b>${days[todayIndex]}</b>`;
+// const day = document.querySelector('.week');
+// const todayDay = new Date();
 
-// document.body.insertAdjacentHTML("beforeEnd", days.join("<br>"));
+// const days = () => {
+//   week.forEach((item, i) => {
+//     let newdiv = document.createElement('div');
+
+//     if (i === 6 + (todayDay.getDay() % 7)) {
+//       console.log(6 + (todayDay.getDay() % 7));
+//       newdiv.classList.add('bold');
+//       newdiv.textContent = week[i];
+//     }
+
+//     if (item === 'Суббота' || item === 'Воскресенье') {
+//       newdiv.classList.add('italic'); // Делаем дополнительно курсивом (Даже если уже жирным выделено)
+//       newdiv.textContent = week[i];
+//     } else {
+//       newdiv.textContent = week[i]; // Если не текущий день и не выходные, то просто вставляем текст без стилей
+//     }
+
+//     day.appendChild(newdiv);
+//   });
+// };
+
+// days();
