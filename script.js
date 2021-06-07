a9a3bcff40007b87faa847cf79444696442b8d50
EAMendiguzhaev <mendiguzhaevea@gmail.com>
2021-06-08 00:26:39 +0300

    Выполняю ДЗ 9 урока

--- script.js
+++ script.js
@@ -1,54 +1,75 @@
 'use strict';
 
-let days = [
-  'Понедельник',
-  'Вторник',
-  'Среда',
-  'Четверг',
-  'Пятница',
-  '<i>Суббота</i>',
-  '<i>Воскресенье</i>',
-];
+// 1) Выведите на страницу текущую дату и время в 2-х форматах:
 
-let todayIndex = (6 + new Date().getDay()) % 7;
-days[todayIndex] = `<b>${days[todayIndex]}</b>`;
+//     a) 'Сегодня Вторник, 4 февраля 2020 года, 21 час 5 минут 33 секунды'
 
-document.body.insertAdjacentHTML('beforeEnd', days.join('<br>'));
+//     б) '04.02.2020 - 21:05:33'
 
-// Другой подход
+// 2) Для вывода в формате (а) напишите функцию, которая будет менять склонение слов в зависимости от числа, "час, часов, часа"
 
-// const week = [
-//   'Понедельник',
-//   'Вторник',
-//   'Среда',
-//   'Четверг',
-//   'Пятница',
-//   'Суббота',
-//   'Воскресенье',
-// ];
+// 3) Для вывода в формате (б) напишите функцию, которая будет добавлять 0 перед значениями которые состоят из одной цифры (из 9:5:3  1.6.2019 сделает 09:05:03 01.06.2019)
 
-// const day = document.querySelector('.week');
-// const todayDay = new Date();
+const fullDate = document.querySelector('.full-date');
+const shortenedDate = document.querySelector('.shortened-date');
+let hour;
+let minutes;
+let seconds;
 
-// const days = () => {
-//   week.forEach((item, i) => {
-//     let newdiv = document.createElement('div');
+const date = new Date();
+const optionsfullDate = {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  weekday: 'long',
+  timezone: 'UTC',
+};
 
-//     if (i === 6 + (todayDay.getDay() % 7)) {
-//       console.log(6 + (todayDay.getDay() % 7));
-//       newdiv.classList.add('bold');
-//       newdiv.textContent = week[i];
-//     }
+function declination(number, words) {
+  return words[number % 100 > 4 && number % 100 < 20 ? 2 : [2, 0, 1, 1, 1, 2][number % 10 < 5 ? number % 10 : 5]];
+}
 
-//     if (item === 'Суббота' || item === 'Воскресенье') {
-//       newdiv.classList.add('italic'); // Делаем дополнительно курсивом (Даже если уже жирным выделено)
-//       newdiv.textContent = week[i];
-//     } else {
-//       newdiv.textContent = week[i]; // Если не текущий день и не выходные, то просто вставляем текст без стилей
-//     }
+const textFullDate = `Cегодня ${date.toLocaleString('ru', optionsfullDate)} 
+  ${(declination(date.getHours()), ['час', 'часов', 'часа'])}
+  ${(declination(date.getMinutes()), ['минут', 'минута', 'минуты'])}
+  ${(declination(date.getSeconds()), ['секунд', 'секунда', 'секунды'])}`;
 
-//     day.appendChild(newdiv);
-//   });
+const optionsShortDate = {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+  timezone: 'UTC',
+};
+
+const getZero = function (num) {
+  if (num > 0 && num < 10) {
+    return '0' + num;
+  } else {
+    return num;
+  }
+};
+
+const textShortenedDate =
+  getZero(date.toLocaleString('ru', optionsShortDate)) +
+  ' ' +
+  getZero(date.getHours()) +
+  ':' +
+  getZero(date.getMinutes()) +
+  ':' +
+  getZero(date.getSeconds());
+
+const getThisYear = () => {
+  const dateFull = textFullDate;
+  const dateShort = textShortenedDate;
+  fullDate.textContent = dateFull;
+  shortenedDate.textContent = dateShort;
+};
+setInterval(getThisYear, 1000);
+
+// Живое время
+// const times = document.querySelector('.time');
+// const time = () => {
+//   const clock = new Date().toLocaleTimeString();
+//   times.textContent = clock;
 // };
-
-// days();
+// setInterval(time, 1000);
