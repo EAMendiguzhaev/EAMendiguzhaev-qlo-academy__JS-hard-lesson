3db378da55d524ab8a854bfea90c2f4bf63cca2c
EAMendiguzhaev <mendiguzhaevea@gmail.com>
2021-06-12 19:34:44 +0300

    Квадрат в пространстве

--- script.js
+++ script.js
@@ -1,80 +1,57 @@
 'use strict';
 
-const fullDate = document.querySelector('.full-date');
-const shortenedDate = document.querySelector('.shortened-date');
-
-const getThisYear = () => {
-  const date = new Date();
-
-  const optionsfullDate = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    weekday: 'long',
-    timezone: 'UTC',
+document.addEventListener('DOMContentLoaded', () => {
+  const DomElement = function (selector, options) {
+    this.selector = selector;
+    options = options || {};
+    this.height = options.height;
+    this.width = options.width;
+    this.bg = options.bg;
+    this.position = options.position;
   };
 
-  function declOfNum(n, text_forms) {
-    n = Math.abs(n) % 100;
-    var n1 = n % 10;
-    if (n > 10 && n < 20) {
-      return text_forms[2];
-    }
-    if (n1 > 1 && n1 < 5) {
-      return text_forms[1];
-    }
-    if (n1 == 1) {
-      return text_forms[0];
-    }
-    return text_forms[2];
-  }
+  DomElement.prototype.render = function () {
+    let div = document.createElement('div');
+    div.classList.add(this.selector);
 
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
-  const arrHours = declOfNum(hours, ['час', 'часа', 'часов']);
-  const arrMinutes = declOfNum(minutes, ['минута', 'минуты', 'минут']);
-  const arrSeconds = declOfNum(seconds, ['секунда', 'секунды', 'секунд']);
-
-  const textFullDate = `Cегодня ${date.toLocaleString('ru', optionsfullDate)} 
-      ${hours} ${arrHours}
-      ${minutes} ${arrMinutes}
-      ${seconds} ${arrSeconds}`;
-
-  const optionsShortDate = {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-    timezone: 'UTC',
+    div.style.cssText = `height: ${this.height};
+                   width: ${this.width};
+                   background-color:  ${this.bg};
+                   font-size: ${this.fontSize};
+                   position: ${this.position}`;
+    document.body.insertAdjacentElement('beforeEnd', div);
   };
 
-  const getZero = function (num) {
-    if (num > 0 && num < 10) {
-      return '0' + num;
-    } else {
-      return num;
+  const element = new DomElement('element', {
+    height: '100px',
+    width: '100px',
+    bg: '#b3ffda',
+    position: 'absolute',
+  });
+  element.render();
+
+  let moveRightAndLeft = 0;
+  let moveUpAndBottom = 0;
+
+  document.addEventListener('keydown', function (event) {
+    let elementBody = document.querySelector('.element');
+
+    if (event.keyCode === 40) {
+      moveUpAndBottom += 10;
+      elementBody.style.marginTop = moveUpAndBottom + 'px';
+      console.log('Вниз');
+    } else if (event.keyCode === 39) {
+      moveRightAndLeft += 10;
+      elementBody.style.marginLeft = moveRightAndLeft + 'px';
+      console.log('Вправо');
+    } else if (event.keyCode === 38) {
+      moveUpAndBottom -= 10;
+      elementBody.style.marginTop = moveUpAndBottom + 'px';
+      console.log('Вверх');
+    } else if (event.keyCode === 37) {
+      moveRightAndLeft -= 10;
+      elementBody.style.marginLeft = moveRightAndLeft + 'px';
+      console.log('Влево');
     }
-  };
-
-  const clock = new Date().toLocaleTimeString();
-  const textShortenedDate = `${getZero(date.toLocaleString('ru', optionsShortDate))} 
-    ${clock}`;
-
-  /* Можно и так:
-    const textShortenedDate = `${getZero(date.toLocaleString('ru', optionsShortDate))} 
-    ${getZero(date.getHours())}:${getZero(date.getMinutes())}:${getZero(date.getSeconds())};`
-    */
-  const dateFull = textFullDate;
-  const dateShort = textShortenedDate;
-  fullDate.textContent = dateFull;
-  shortenedDate.textContent = dateShort;
-};
-setInterval(getThisYear, 1000);
-
-// Живое время
-// const times = document.querySelector('.time');
-// const time = () => {
-//   const clock = new Date().toLocaleTimeString();
-//   times.textContent = clock;
-// };
-// setInterval(time, 1000);
+  });
+});
